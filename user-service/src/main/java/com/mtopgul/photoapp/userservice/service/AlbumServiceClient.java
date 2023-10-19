0baa5a46543fea660379139c7f875dee0370456a
mtopgul <muhammedtopgul.29@gmail.com>
2023-10-19 17:46:31 +0300

    Resilience4J CircuitBreaker implementing.

--- user-service/src/main/java/com/mtopgul/photoapp/userservice/service/AlbumServiceClient.java
+++ user-service/src/main/java/com/mtopgul/photoapp/userservice/service/AlbumServiceClient.java
@@ -1,11 +1,14 @@
 package com.mtopgul.photoapp.userservice.service;
 
 import com.mtopgul.photoapp.userservice.dto.AlbumDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * @author muhammed-topgul
@@ -13,6 +16,15 @@ import java.util.List;
  */
 @FeignClient(name = "album-service")
 public interface AlbumServiceClient {
+    Logger log = Logger.getLogger("AlbumServiceClient");
+
     @GetMapping("/api/albums/{id}")
+    @CircuitBreaker(name = "album-service", fallbackMethod = "getAlbumsFallback")
     List<AlbumDto> getAlbums(@PathVariable(name = "id") String id);
+
+    default List<AlbumDto> getAlbumsFallback(String id, Throwable exception) {
+        log.info("Param " + id);
+        log.info("Exception took place: " + exception.getMessage());
+        return new ArrayList<>();
+    }
 }
