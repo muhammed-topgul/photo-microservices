37f74ddef5c031c27a9cf5087bf6554fbe945c82
mtopgul <muhammedtopgul.29@gmail.com>
2023-10-24 15:09:38 +0300

    Dockerfile config.

--- .env/build.bat
+++ .env/build.bat
@@ -4,33 +4,42 @@ set PROJECT_DIR=%~dp0\..
 call cd %PROJECT_DIR%\discovery-service
 call mvn clean
 call mvn install
-@REM call docker build --no-cache . -t mtopgul/discovery-service
+call docker build --no-cache . -t mtopgul/discovery-service --force-rm=true
 call cd %PROJECT_DIR%
 
 :::::::::::::::::::::::::::::Config Server::::::::::::::::::::::::::::::
 call cd %PROJECT_DIR%\config-server
 call mvn clean
 call mvn install
-@REM call docker build --no-cache . -t mtopgul/user-service
+call docker build --no-cache . -t mtopgul/user-service --force-rm=true
 call cd %PROJECT_DIR%
 
 :::::::::::::::::::::::::::::Account Service::::::::::::::::::::::::::::::
 call cd %PROJECT_DIR%\account-service
 call mvn clean
 call mvn install
-@REM call docker build --no-cache . -t mtopgul/account-service
+call docker build --no-cache . -t mtopgul/account-service --force-rm=true
+call cd %PROJECT_DIR%
+
+:::::::::::::::::::::::::::::Discovery Service::::::::::::::::::::::::::::::
+call cd %PROJECT_DIR%\discovery-service
+call mvn clean
+call mvn install
+call docker build --no-cache . -t mtopgul/discovery-service --force-rm=true
 call cd %PROJECT_DIR%
 
 :::::::::::::::::::::::::::::User Service::::::::::::::::::::::::::::::
 call cd %PROJECT_DIR%\user-service
 call mvn clean
 call mvn install
-@REM call docker build --no-cache . -t mtopgul/user-service
+call docker build --no-cache . -t mtopgul/user-service --force-rm=true
 call cd %PROJECT_DIR%
 
 :::::::::::::::::::::::::::::API Gateway Service::::::::::::::::::::::::::::::
 call cd %PROJECT_DIR%\api-gateway
 call mvn clean
 call mvn install
-@REM call docker build --no-cache . -t mtopgul/api-gateway
+call docker build --no-cache . -t mtopgul/api-gateway --force-rm=true
 call cd %PROJECT_DIR%
+
+pause
