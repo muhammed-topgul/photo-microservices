27f6e19c48ad2c68fefacb03a51a96b3bdb290c9
mtopgul <muhammedtopgul.29@gmail.com>
2023-10-25 16:38:46 +0300

    Build bat file updated.

--- .env/build.bat
+++ .env/build.bat
@@ -1,24 +1,31 @@
 set PROJECT_DIR=%~dp0\..
 
-:::::::::::::::::::::::::::::Discovery Service::::::::::::::::::::::::::::::
-call cd %PROJECT_DIR%\discovery-service
+:::::::::::::::::::::::::::::Account Service::::::::::::::::::::::::::::::
+call cd %PROJECT_DIR%\account-service
 call mvn clean
 call mvn install
-call docker build --no-cache . -t mtopgul/discovery-service --force-rm=true
+call docker build --no-cache . -t mtopgul/account-service --force-rm=true
+call cd %PROJECT_DIR%
+
+:::::::::::::::::::::::::::::Album Service::::::::::::::::::::::::::::::
+call cd %PROJECT_DIR%\album-service
+call mvn clean
+call mvn install
+call docker build --no-cache . -t mtopgul/album-service --force-rm=true
+call cd %PROJECT_DIR%
+
+:::::::::::::::::::::::::::::API Gateway Service::::::::::::::::::::::::::::::
+call cd %PROJECT_DIR%\api-gateway
+call mvn clean
+call mvn install
+call docker build --no-cache . -t mtopgul/api-gateway --force-rm=true
 call cd %PROJECT_DIR%
 
 :::::::::::::::::::::::::::::Config Server::::::::::::::::::::::::::::::
 call cd %PROJECT_DIR%\config-server
 call mvn clean
 call mvn install
-call docker build --no-cache . -t mtopgul/user-service --force-rm=true
-call cd %PROJECT_DIR%
-
-:::::::::::::::::::::::::::::Account Service::::::::::::::::::::::::::::::
-call cd %PROJECT_DIR%\account-service
-call mvn clean
-call mvn install
-call docker build --no-cache . -t mtopgul/account-service --force-rm=true
+call docker build --no-cache . -t mtopgul/config-service --force-rm=true
 call cd %PROJECT_DIR%
 
 :::::::::::::::::::::::::::::Discovery Service::::::::::::::::::::::::::::::
@@ -33,13 +40,6 @@ call cd %PROJECT_DIR%\user-service
 call mvn clean
 call mvn install
 call docker build --no-cache . -t mtopgul/user-service --force-rm=true
-call cd %PROJECT_DIR%
-
-:::::::::::::::::::::::::::::API Gateway Service::::::::::::::::::::::::::::::
-call cd %PROJECT_DIR%\api-gateway
-call mvn clean
-call mvn install
-call docker build --no-cache . -t mtopgul/api-gateway --force-rm=true
 call cd %PROJECT_DIR%
 
 pause
