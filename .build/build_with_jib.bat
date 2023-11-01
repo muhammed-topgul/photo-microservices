28ae5136aa83640578ad3c43f6df269d1ac10d9a
muhammed-topgul <muhammedtopgul.29@gmail.com>
2023-11-01 23:20:28 +0300

    Eureka cluster created.

--- .build/build_with_jib.bat
+++ .build/build_with_jib.bat
@@ -10,42 +10,42 @@ call cd %PROJECT_DIR%
 call cd %PROJECT_DIR%\account-service
 call mvn clean
 call mvn install
-
+call mvn jib:dockerBuild
 call cd %PROJECT_DIR%
 
 :::::::::::::::::::::::::::::Album Service::::::::::::::::::::::::::::::
 call cd %PROJECT_DIR%\album-service
 call mvn clean
 call mvn install
-
+call mvn jib:dockerBuild
 call cd %PROJECT_DIR%
 
 :::::::::::::::::::::::::::::API Gateway Service::::::::::::::::::::::::::::::
 call cd %PROJECT_DIR%\api-gateway
 call mvn clean
 call mvn install
-
+call mvn jib:dockerBuild
 call cd %PROJECT_DIR%
 
 :::::::::::::::::::::::::::::Config Server::::::::::::::::::::::::::::::
 call cd %PROJECT_DIR%\config-server
 call mvn clean
 call mvn install
-
+call mvn jib:dockerBuild
 call cd %PROJECT_DIR%
 
 :::::::::::::::::::::::::::::Discovery Service::::::::::::::::::::::::::::::
 call cd %PROJECT_DIR%\discovery-service
 call mvn clean
 call mvn install
-
+call mvn jib:dockerBuild
 call cd %PROJECT_DIR%
 
 :::::::::::::::::::::::::::::User Service::::::::::::::::::::::::::::::
 call cd %PROJECT_DIR%\user-service
 call mvn clean
 call mvn install
-
+call mvn jib:dockerBuild
 call cd %PROJECT_DIR%
 
 pause
